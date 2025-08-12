6dcc58c77cadce8eab0cb102adce08efe5d0a891
Abhay-Pratap-Singh-7 <abhaypratapsingh2993@gmail.com>
2025-08-12 16:29:25 +0530

    Integer and Float

---
+++ Integer_Float.py
@@ -0,0 +1,28 @@
+# To know type of variable
+num = 3
+print(type(num))
+num = 3.14
+print(type(num))
+
+print( 3 + 2 )
+print( 3 - 2 )
+print( 3 * 2 )
+print( 3 / 2 )
+# Truncate decimal part
+print( 3 // 2 )
+# 3 to the power 2
+print( 3 ** 2 )
+# provide remainder
+print( 3 % 2 )
+
+# To get absolute value i.e value without any sign
+num = -3
+print(abs(num))
+
+# to round-off any value , second arugment is for how many digit after dcimal you want
+print(round(3.14, 1))
+
+# convert string to int
+num = '100'
+num_2 = '200'
+print(num + num_2)
