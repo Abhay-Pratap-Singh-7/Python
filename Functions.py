bc7acc056e8e9b204f91f88bf75d1a97bb619bc1
Abhay-Pratap-Singh-7 <abhaypratapsingh2993@gmail.com>
2025-08-13 22:19:39 +0530

    Function

---
+++ Functions.py
@@ -0,0 +1,13 @@
+# Declare function
+def hello():
+    print('Hello Function')
+    return
+
+#Calling the function
+hello()
+
+# Passing arugment in function
+def sum(a, b):
+    return a+b
+
+print(sum(3, 5))
